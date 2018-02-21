db67b788be009da193ba63ad3bbbdff3e9bb8089
Colter-Hammer <therondowdle7@gmail.com>
2018-02-21 13:34:02 -0700

    Added an option for amount of variation

--- manyColors.js
+++ manyColors.js
@@ -1,7 +1,7 @@
 /**=======================================================================
 * Create the array of different colors
 
-* 1. git the value of the input and save it to an array with .split()
+* 1. get the value of the input and save it to an array with .split()
 * 2. forEach the array
 *      a. inside the forEach use tinycolor to get all the properties of the given colors, and push them to an array. 
 * 3. Call a function that gets the 6 variations of the colors given
@@ -12,54 +12,51 @@
 
 
 ****do tomorrow****
-* Add comments
 * give the user an option of how many different color variations to output
 * change map to use the number of variations
 ========================================================================= */
 /* eslint no-unused-vars:0 */
+/* eslint no-console:0 */
+/* eslint quotes: 1 */
+
+// receives as input a color, and returns an array of variations of those colors using the tinycolor.monochromatic library. Sorted colors from darkest to lightest.
 function convertColor(colorValue) {
-       
-    let colorArray = tinycolor(colorValue)
-        // gets arrays of colors based on
-        .monochromatic(12)
-        .sort((a, b) => a.getLuminance() - b.getLuminance())
-        // lsdfjlskd
-        .map(color => color.toHexString());
+    let amount = document.querySelector('#colorVariation').value,
+        colorArray = tinycolor(colorValue)
+        // gets arrays of colors from library allowing
+            .monochromatic(amount)
+            // sort colors from monochromatic from dark to light
+            .sort((a, b) => a.getLuminance() - b.getLuminance())
+            // change from previous format to hex
+            .map(color => color.toHexString());
 
     return colorArray;
 }
 
-function makeColorDivHTML(colorList){
+function makeColorDivHTML(colorList) {
     let htmlOut = '<div class="colors">';
     // make the html for each color in the array and then join to one big string
     htmlOut += colorList
-        .map(color => `<div class="color" style="background-color: ${color};" >${color}</div>`)
+        .map(color => `<div class="color" style="background-color: ${color};">${color}</div>`)
         .join('');
 
-
+    // Add the created html tag to the div container
     htmlOut += '</div>';
     return htmlOut;
 }
-
-
-
-console.log('end:', convertColor('red'));
-
-// // Variables
-// let colors;
-// let tiny;
-// let variation;
-// let arr;
-
 
 function main() {
     let htmlString,
         colors = document.querySelector('#colorInput').value.split('\n');
 
-    // add colors an array that uses the tinycolor format.
     htmlString = colors
+        // Returns a multilayered array of the hex codes for the colors and variations
         .map(convertColor)
+
+        // Returns a single array containing all the divs created in makeColorDivHTML function
         .map(makeColorDivHTML)
+
+        // Takes the array and joins it into one long string without the commas
         .join('');
 
     document.querySelector(`#colorSquares`).innerHTML = htmlString;
@@ -71,6 +68,13 @@ function main() {
 }
 
 document.querySelector('#things').addEventListener('click', main);
+
+
+// // Variables
+// let colors;
+// let tiny;
+// let variation;
+// let arr;
 
 // // monochromatic returns 6 variations of the colors given
 // function monochromatic(color) {
