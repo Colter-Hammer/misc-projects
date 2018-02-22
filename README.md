449d526c7cd021f88b5db9b1ccb3cea4be617190
colter-hammer <therondowdle7@gmail.com>
2018-02-21 18:32:49 -0700

    Completed the basics. Monochromatic works and displays

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# misc-projects
-
-## colors.html
-
-* Has the user input 4 colors separated by commas and shows 6 variations of those colors in separate divs.
-    * The text of the div will be the hex code of the variations. 
-    * Dependent on tinycolors
+README
--- manyColors.js
+++ manyColors.js
@@ -1,23 +1,22 @@
-/**=======================================================================
-* Create the array of different colors
+// /**=======================================================================
+// * Create the array of different colors
 
-* 1. get the value of the input and save it to an array with .split()
-* 2. forEach the array
-*      a. inside the forEach use tinycolor to get all the properties of the given colors, and push them to an array. 
-* 3. Call a function that gets the 6 variations of the colors given
-*      a. Use monochromatic to create the variations
-*      b. return an array of arrays consisting of the different colors
-* 4. Call a function to set the colors to the div elements in the HTML
-*      a. 
+// * 1. get the value of the input and save it to an array with .split()
+// * 2. forEach the array  
+// *      a. inside the forEach use tinycolor to get all the properties of the given colors, and push them to an array. 
+// * 3. Call a function that gets the 6 variations of the colors given
+// *      a. Use monochromatic to create the variations  
+// *      b. return an array of arrays consisting of the different colors  
+// * 4. Call a function to set the colors to the div elements in the HTML  
+// *      a. 
 
 
-****do tomorrow****
-* give the user an option of how many different color variations to output
-* change map to use the number of variations
-========================================================================= */
-/* eslint no-unused-vars:0 */
-/* eslint no-console:0 */
-/* eslint quotes: 1 */
+// ****do tomorrow****
+// * give the user an option of how many different color variations to output
+// * change map to use the number of variations
+// ========================================================================= */
+/* eslint no-unused-vars:0, no-console:0, quotes: 1 */
+
 
 // receives as input a color, and returns an array of variations of those colors using the tinycolor.monochromatic library. Sorted colors from darkest to lightest.
 function convertColor(colorValue) {
@@ -25,6 +24,7 @@ function convertColor(colorValue) {
         colorArray = tinycolor(colorValue)
         // gets arrays of colors from library allowing
             .monochromatic(amount)
+            // .tetrad(amount)
             // sort colors from monochromatic from dark to light
             .sort((a, b) => a.getLuminance() - b.getLuminance())
             // change from previous format to hex
@@ -61,55 +61,7 @@ function main() {
 
     document.querySelector(`#colorSquares`).innerHTML = htmlString;
 
-
     console.log(htmlString);
-
-
 }
 
 document.querySelector('#things').addEventListener('click', main);
-
-
-// // Variables
-// let colors;
-// let tiny;
-// let variation;
-// let arr;
-
-// // monochromatic returns 6 variations of the colors given
-// function monochromatic(color) {
-//     variation = [];
-//     color.forEach(color => {
-//         variation.push(color.monochromatic());
-//     });
-//     return variation;
-// }
-
-// // change from HSV to hex
-// function toHex(array) {
-//     arr = [];
-//     for (let i = 0; i < array.length; i++) {
-//         arr[i] = array[i].map(hsv => hsv.toHexString());
-//         // console.log(arr[i]);
-//     }
-//     return arr;
-// }
-
-// // apply hex code to div background color
-// function applyHex(arr) {
-//     for (let j = 0; j < tiny.length; j++) {
-//         for (let i = 0; i < 6; i++) {
-//             document.querySelector(`.color${[j+1]}-${[i+1]}`).style.backgroundColor = arr[j][i];
-//             document.querySelector(`.color${[j+1]}-${[i+1]}`).innerHTML = arr[j][i];
-//         }
-//     }
-// }
-
-// // function reset() {
-// //     for (let j = 0; j < tiny.length; j++) {
-// //         for (let i = 0; i < 6; i++) {
-// //             document.querySelector(`.color${[j+1]}-${[i+1]}`).style.backgroundColor = 'white';
-// //             document.querySelector(`.color${[j+1]}-${[i+1]}`).innerHTML = '';
-// //         }
-// //     }
-// // }
